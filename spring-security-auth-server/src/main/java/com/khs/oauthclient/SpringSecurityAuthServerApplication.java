e328c4253018e0a7f56be045e4c328acf7f85d69
Himanshu Shukla <himanshu.shukla@nw18.com>
2020-05-06 21:07:06 +0530

    OAuth 2.0 Server and client APP

---
+++ spring-security-auth-server/src/main/java/com/khs/oauthclient/SpringSecurityAuthServerApplication.java
@@ -0,0 +1,14 @@
+package com.khs.oauthclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@SpringBootApplication
+@EnableResourceServer
+public class SpringSecurityAuthServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAuthServerApplication.class, args);
+	}
+
+}
---
+++ spring-security-client/src/main/java/com/khs/oauthclient/SpringSecurityClientApplication.java
@@ -0,0 +1,13 @@
+package com.khs.oauthclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityClientApplication.class, args);
+	}
+
+}
